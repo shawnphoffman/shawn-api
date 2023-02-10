fb081f188db09d24b9164259309c11df1f3cab22
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2023-02-10 11:54:38 -0800

    add github webhook

---
+++ src/pages/api/webhooks/github/blue-harvest.js
@@ -0,0 +1,10 @@
+// import Cors from 'src/utils/cors'
+
+export default async function handler(req, res) {
+	// await Cors(req, res, {
+	// 	methods: ['GET', 'OPTIONS'],
+	// 	origin: [/shawn\.party$/],
+	// })
+
+	res.status(200).json({ blue: 'harvest' })
+}

784b6485ca22a9ea94be44f18ca9c71dfbbcac0a
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2023-02-13 22:21:55 -0800

    Update garbage book api because it too is dumb

--- src/pages/api/star-wars/future-books.js
+++ src/pages/api/star-wars/future-books.js
@@ -2,6 +2,65 @@ import { fetchHtmlWithCache } from '@/utils/fetchWithCache'
 import * as cheerio from 'cheerio'
 
 const url = 'https://starwars.fandom.com/wiki/List_of_future_books'
+const altUrl = 'https://starwars.fandom.com/wiki/Timeline_of_canon_books'
+
+const getBooksAlt = async () => {
+	var yesterday = new Date()
+	yesterday.setDate(yesterday.getDate() - 2)
+
+	const requestOptions = {
+		method: 'GET',
+	}
+	const data = await fetchHtmlWithCache(altUrl, requestOptions, 60 * 24)
+
+	const $ = cheerio.load(data)
+
+	const books = $('table.wikitable tbody tr')
+		.map(function (i) {
+			if (i === 0) return null
+
+			const valueMap = $(this)
+				.find('td')
+				.map(function (j, td) {
+					return $(td).text().trim()
+				})
+
+			// Grab a link to the book
+			const linkObj = $($(this).find('td')[2]).find('a:first')
+			const title = linkObj.text().trim() || $($(this).find('td')[2]).find('span').text().trim()
+			const link = linkObj.prop('href')
+
+			const rawDate = valueMap[4]
+			if (!rawDate) {
+				// console.log(`missing date: ${i} ${rawDate} - ${title}`)
+				return null
+			}
+
+			const date = new Date(rawDate)
+			if (date < yesterday) {
+				return null
+			}
+
+			// Grab the comic data
+			const comic = {
+				title: title,
+				type: valueMap[1],
+				pubDate: date,
+				url: link,
+				author: valueMap[3],
+			}
+
+			return comic
+		})
+		.toArray()
+		.filter(i => i !== null)
+		.sort((a, b) => a.pubDate - b.pubDate)
+
+	console.log('-----------------')
+	console.log(`ALT BOOK COUNT: ${books.length}`)
+
+	return books
+}
 
 export async function getBooks() {
 	const requestOptions = {
@@ -58,6 +117,20 @@ export async function getBooks() {
 
 export default async function handler(req, res) {
 	const response = await getBooks()
+	const response2 = await getBooksAlt()
 
-	res.status(200).send(response)
+	const alt = [...response2, ...response].reduce((memo, el) => {
+		// if (memo[el.title]) {
+
+		// }
+		memo[el.title] = {
+			...memo[el.title],
+			...el,
+		}
+		return memo
+	}, {})
+
+	const temp = Object.values(alt).sort((a, b) => a.pubDate - b.pubDate)
+
+	res.status(200).send(temp)
 }

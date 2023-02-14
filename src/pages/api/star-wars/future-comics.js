555ddde269bb96e7851d1ab84ce023818de853ca
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2023-02-13 22:18:02 -0800

    Update comics APII because it is dumb

--- src/pages/api/star-wars/future-comics.js
+++ src/pages/api/star-wars/future-comics.js
@@ -1,7 +1,68 @@
 import { fetchHtmlWithCache } from '@/utils/fetchWithCache'
 import * as cheerio from 'cheerio'
 
+// NOTE This is ridiculously inefficient
+
 const url = 'https://starwars.fandom.com/wiki/List_of_future_comics'
+const altUrl = 'https://starwars.fandom.com/wiki/Timeline_of_canon_comics'
+
+const getComicsAlt = async () => {
+	var yesterday = new Date()
+	yesterday.setDate(yesterday.getDate() - 2)
+
+	const requestOptions = {
+		method: 'GET',
+	}
+	const data = await fetchHtmlWithCache(altUrl, requestOptions, 60 * 24)
+
+	const $ = cheerio.load(data)
+
+	const comics = $('table.wikitable tbody tr')
+		.map(function (i) {
+			if (i === 0) return null
+
+			const valueMap = $(this)
+				.find('td')
+				.map(function (j, td) {
+					return $(td).text().trim()
+				})
+
+			// Grab a link to the comic
+			const linkObj = $($(this).find('td')[2]).find('a:first')
+			const title = linkObj.text().trim() || $($(this).find('td')[2]).find('span').text().trim()
+			const link = linkObj.prop('href')
+
+			const rawDate = valueMap[4]
+			if (!rawDate) {
+				// console.log(`missing date: ${i} ${rawDate} - ${title}`)
+				return null
+			}
+
+			const date = new Date(rawDate)
+			if (date < yesterday) {
+				return null
+			}
+
+			// Grab the comic data
+			const comic = {
+				title: title,
+				type: valueMap[1],
+				pubDate: date,
+				url: link,
+				author: valueMap[3],
+			}
+
+			return comic
+		})
+		.toArray()
+		.filter(i => i !== null)
+		.sort((a, b) => a.pubDate - b.pubDate)
+
+	console.log('-----------------')
+	console.log(`ALT COMIC COUNT: ${comics.length}`)
+
+	return comics
+}
 
 export async function getComics() {
 	const requestOptions = {
@@ -58,6 +119,20 @@ export async function getComics() {
 
 export default async function handler(req, res) {
 	const response = await getComics()
+	const response2 = await getComicsAlt()
 
-	res.status(200).send(response)
+	const alt = [...response2, ...response].reduce((memo, el) => {
+		// if (memo[el.title]) {
+
+		// }
+		memo[el.title] = {
+			...memo[el.title],
+			...el,
+		}
+		return memo
+	}, {})
+
+	const temp = Object.values(alt).sort((a, b) => a.pubDate - b.pubDate)
+
+	res.status(200).send(temp)
 }

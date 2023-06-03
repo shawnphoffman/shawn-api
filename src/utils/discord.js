4280ef24ff8f2aacbd75a82448fa220ef97b6b67
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2023-06-03 11:44:43 -0700

    Discord util

--- src/utils/discord.js
+++ src/utils/discord.js
@@ -20,3 +20,29 @@ export const crossPostMessage = async (channelId, messageId) => {
 		console.error(e)
 	}
 }
+
+export const sendWebhook = async (url, content, crossPost) => {
+	var myHeaders = new Headers()
+	myHeaders.append('Content-Type', 'application/json')
+
+	var requestOptions = {
+		headers: myHeaders,
+		method: 'POST',
+		body: JSON.stringify(content),
+	}
+
+	const response = await fetch(url, requestOptions)
+
+	console.log('-----------------')
+	console.log('WEBHOOK RESPONSE')
+	console.log(`Status: ${response.status}`)
+	console.log(`Status Text: ${response.statusText}`)
+	try {
+		const json = await response.json()
+		if (crossPost) {
+			if (json && json.id && json.channel_id && json.author?.bot) {
+				await crossPostMessage(json.channel_id, json.id)
+			}
+		}
+	} catch (e) {}
+}

431b67bc442f503c25eafa1db8f456cfde63dbd2
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-03-10 19:53:56 -0700

    wat

---
+++ src/pages/api/bsky/force.js
@@ -0,0 +1,30 @@
+import { postBleet } from '@/components/bluesky/bluesky'
+// import { getOgImageUrl } from '@/components/bluesky/imageUtils'
+
+export default async function handler(req, res) {
+	if (req.method !== 'POST') {
+		res.status(405).send({ message: 'Only POST requests allowed' })
+		return
+	}
+
+	const { type, title, content, url, token } = req.body
+
+	if (token !== process.env.PURPLE_API_KEY) {
+		res.status(401).send({ message: 'Unauthorized' })
+		return
+	}
+
+	try {
+		postBleet({ contentType: type, title: title, items: content, url })
+		// postBleet({ contentType: 'ContentType', items: 'TEST (composite) #StorWors', url: scrape })
+
+		// const imageUrl = await getOgImageUrl(scrape)
+		// if (imageUrl) {
+		// 	return res.status(200).send({ imageUrl })
+		// }
+		// return res.status(404).send('No image found')
+		return res.status(200).send('OK')
+	} catch (error) {
+		return res.status(400).send('Bad request')
+	}
+}

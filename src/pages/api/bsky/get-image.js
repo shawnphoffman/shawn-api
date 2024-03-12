8786ac22b1e76b65fa334900fc58191bf15f0d40
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-03-12 07:48:55 -0700

    og image endpoint

--- src/pages/api/bsky/get-image.js
+++ src/pages/api/bsky/get-image.js
@@ -1,5 +1,5 @@
-import { postBleet } from '@/components/bluesky/bluesky'
-// import { getOgImageUrl } from '@/components/bluesky/imageUtils'
+// import { postBleet } from '@/components/bluesky/bluesky'
+import { getOgImageUrl } from '@/components/bluesky/imageUtils'
 
 export default async function handler(req, res) {
 	const scrape = req.query.scrape
@@ -11,14 +11,14 @@ export default async function handler(req, res) {
 	}
 
 	try {
-		postBleet({ contentType: 'ContentType', items: 'TEST (composite) #StorWors', url: scrape })
+		// postBleet({ contentType: 'ContentType', items: 'TEST (composite) #StorWors', url: scrape })
 
-		// const imageUrl = await getOgImageUrl(scrape)
-		// if (imageUrl) {
-		// 	return res.status(200).send({ imageUrl })
-		// }
-		// return res.status(404).send('No image found')
-		return res.status(200).send('OK')
+		const imageUrl = await getOgImageUrl(scrape)
+		if (imageUrl) {
+			return res.status(200).send({ imageUrl })
+		}
+		return res.status(404).send('No image found')
+		// return res.status(200).send('OK')
 	} catch (error) {
 		return res.status(400).send('Bad request')
 	}

f60182e16a48a8d7c45ad66c1994f515789f4f14
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-03-30 13:37:00 -0700

    scripts and ts

--- src/utils/redis.ts
+++ src/utils/redis.ts
@@ -6,8 +6,8 @@ export const RedisKey = {
 }
 
 const redis = new Redis({
-	url: process.env.UPSTASH_REDIS_REST_URL,
-	token: process.env.UPSTASH_REDIS_REST_TOKEN,
+	url: process.env.UPSTASH_REDIS_REST_URL!,
+	token: process.env.UPSTASH_REDIS_REST_TOKEN!,
 })
 
 export default redis

dacd3fc09407769f1e77237990928fee3493bbfb
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-05-15 10:29:57 -0700

    Update AQI defs

--- src/app/api/tahome/purple/route.ts
+++ src/app/api/tahome/purple/route.ts
@@ -36,29 +36,36 @@ function aqiFromPM(pm: number): number | '-' | undefined {
 	if (pm == undefined) return '-'
 	if (pm < 0) return pm
 	if (pm > 1000) return '-'
-	/*                                  AQI         RAW PM2.5
-		Good                               0 - 50   |   0.0 – 12.0
+	/*                                       AQI     RAW PM2.5
+		Good                                0 - 50  |  0.0 – 12.0
 		Moderate                          51 - 100  |  12.1 – 35.4
 		Unhealthy for Sensitive Groups   101 – 150  |  35.5 – 55.4
 		Unhealthy                        151 – 200  |  55.5 – 150.4
 		Very Unhealthy                   201 – 300  |  150.5 – 250.4
 		Hazardous                        301 – 400  |  250.5 – 350.4
 		Hazardous                        401 – 500  |  350.5 – 500.4
-		*/
-	if (pm > 350.5) {
-		return calcAQI(pm, 500, 401, 500.4, 350.5) //Hazardous
-	} else if (pm > 250.5) {
-		return calcAQI(pm, 400, 301, 350.4, 250.5) //Hazardous
+	*/
+	/* UPDATED 2024 */
+	/*                                       AQI     RAW PM2.5
+		Good                                0 - 50  |  0.0 – 9.0
+		Moderate                          51 - 100  |  9.1 – 35.4
+		Unhealthy for Sensitive Groups   101 – 150  |  35.5 – 55.4
+		Unhealthy                        151 – 200  |  55.5 – 125.4
+		Very Unhealthy                   201 – 300  |  125.5 – 225.4
+		Hazardous                        301 – 400  |  225.5 – 500.4
+	*/
+	if (pm > 250.5) {
+		return calcAQI(pm, 500, 301, 500.4, 225.5) //Hazardous
 	} else if (pm > 150.5) {
-		return calcAQI(pm, 300, 201, 250.4, 150.5) //Very Unhealthy
+		return calcAQI(pm, 300, 201, 225.4, 125.5) //Very Unhealthy
 	} else if (pm > 55.5) {
-		return calcAQI(pm, 200, 151, 150.4, 55.5) //Unhealthy
+		return calcAQI(pm, 200, 151, 125.4, 55.5) //Unhealthy
 	} else if (pm > 35.5) {
 		return calcAQI(pm, 150, 101, 55.4, 35.5) //Unhealthy for Sensitive Groups
 	} else if (pm > 12.1) {
-		return calcAQI(pm, 100, 51, 35.4, 12.1) //Moderate
+		return calcAQI(pm, 100, 51, 35.4, 9.1) //Moderate
 	} else if (pm >= 0) {
-		return calcAQI(pm, 50, 0, 12, 0) //Good
+		return calcAQI(pm, 50, 0, 9, 0) //Good
 	} else {
 		return undefined
 	}

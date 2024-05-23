5adedb7401109f2b9dbebc7e8c61c11db7726109
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-05-23 09:24:58 -0400

    Add proxy endpoint

---
+++ src/app/api/webhooks/sanity-relay/route.ts
@@ -0,0 +1,37 @@
+import { type NextRequest, NextResponse } from 'next/server'
+
+const ProxyEndpoints = [
+	// 'http://localhost:3000/api/revalidate',
+	'https://justshillin.com/api/revalidate',
+	'https://dev.justshillin.com/api/revalidate',
+	'https://scruffypod.com/api/revalidate',
+	'https://dev.scruffypod.com/api/revalidate',
+]
+
+export async function POST(req: NextRequest) {
+	//
+	const bodyText = await req.text()
+	//
+	const responses: { endpoint: string; status: string; message?: string; misc?: any; statusText?: string }[] = []
+
+	for (const endpoint of ProxyEndpoints) {
+		try {
+			const response = await fetch(endpoint, {
+				method: 'POST',
+				body: bodyText,
+				headers: req.headers,
+			})
+			if (response.ok) {
+				responses.push({ endpoint, status: 'success' })
+			} else {
+				responses.push({ endpoint, status: 'error', message: 'Invalid response status', misc: response, statusText: response.statusText })
+				console.error(`Endpoint Error: ${endpoint}`, response.statusText, response)
+			}
+		} catch (err) {
+			responses.push({ endpoint, status: 'exception', message: err.message })
+			console.error(`Endpoint Error: ${endpoint}`, err)
+		}
+	}
+
+	return NextResponse.json({ responses })
+}

4e1f01c42c0040082afe8b4f598c799ff76ced5c
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-06-03 17:01:02 -0400

    Remove pointless waiting

--- src/third-party/notifiers/urls.ts
+++ src/third-party/notifiers/urls.ts
@@ -6,7 +6,7 @@ export const pingRefreshUrls = async (feedName: string, urls: string[]) => {
 		for (const url of urls) {
 			const response = await fetch(url)
 			console.log(` + ${response.status} - Ping Refresh URL: ${url}`)
-			await new Promise(resolve => setTimeout(resolve, 2000))
+			// await new Promise(resolve => setTimeout(resolve, 2000))
 		}
 	} catch (error) {
 		// Sentry.captureException(error)

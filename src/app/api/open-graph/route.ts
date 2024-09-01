c8d9b7a7263f25dba72f790de9fd4c0dc833d71b
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-09-01 10:14:33 -0700

    user agent

--- src/app/api/open-graph/route.ts
+++ src/app/api/open-graph/route.ts
@@ -3,6 +3,12 @@ import { AxiomRequest, withAxiom } from 'next-axiom'
 
 const temp = 'https://scruffypodcasters.podbean.com/e/ep-250-3-quid-each/'
 
+const UserAgents = {
+	FacebookBot: 'facebookexternalhit/1.1',
+	Generic: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
+	GoogleBot: 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
+}
+
 const readMT = (el: any, name: string) => {
 	var prop = el.attr('name') || el.attr('property')
 	return prop == name ? el.attr('content') : null
@@ -22,8 +28,12 @@ export const GET = withAxiom(async (req: AxiomRequest) => {
 		const res = await fetch(scrape, {
 			method: 'GET',
 			next: { revalidate: 300 },
+			headers: {
+				'User-Agent': UserAgents.Generic,
+			},
 		})
 		const data = await res.text()
+
 
 		const $ = cheerio.load(data)
 
